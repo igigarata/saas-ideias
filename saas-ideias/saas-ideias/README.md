16dd86d028dc5204a2cefbf36400ce0fd71abfc0
igigarata <hypevibefesta@gmail.com>
2025-01-29 15:52:06 +0000

    mudanca

---
+++ saas-ideias/saas-ideias/README.md
@@ -0,0 +1,3 @@
+# SaaS Ideias
+
+Projeto de exemplo para gerenciamento de ideias usando Next.js e Tailwind CSS.
---
+++ saas-ideias/saas-ideias/pages/index.js
@@ -0,0 +1,9 @@
+import IdeaManagement from "@/components/IdeaManagement";
+
+export default function Home() {
+  return (
+    <div>
+      <IdeaManagement />
+    </div>
+  );
+}
